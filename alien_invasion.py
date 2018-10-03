d225e2b653d452b8b767ea25d2ffbf2fbc3d34d6
Pedro Barreto <pedrobarreto.ti@outlook.com>
2018-10-03 10:47:01 -0300

    Instancia de Ship. set do icone do jogo. refacotr de funÃÃes

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,32 +2,35 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
+import game_funcions as gf
 
 
 def run_game():
     # Inicia o jogo e cria um objeto para a tela
     pygame.init()
     ai_settings = Settings()
+    ai_settings.set_icon("alien_icon_32x32.bmp")
 
+    # cria uma tela com as dimensões de ai_settings
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height)
     )
     pygame.display.set_caption("Alien Invasion")
 
+    # cria a espaçonave
+    ship = Ship(ai_settings, screen)
+
     # Inicia o laço principal do jogo
     # neste onde ocorre todos os eventos
     while True:
 
-        # realiza escuta de eventos
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        
-        # seta a cor na tela
-        screen.fill(ai_settings.bg_color)
-            
-        # Atualiza a tela, deixando a tela mais recente visivel
-        pygame.display.flip()
+        # escuta de eventos de mouse ou teclado
+        gf.check_events(ship)
+        # atualiza a posição da nave
+        ship.update()
+        # atualiza as informações da tela
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
