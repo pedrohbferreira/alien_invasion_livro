f66a4495b2330fb6527e349933341eaded54910a
Pedro Barreto <pedrobarreto.ti@outlook.com>
2018-10-03 18:11:36 -0300

    intancia e renderezação dos aliens na tela

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,9 +4,9 @@ import sys
 import pygame
 from pygame.sprite import Group
 
+import game_funcions as gf
 from settings import Settings
 from ship import Ship
-import game_funcions as gf
 
 
 def run_game():
@@ -15,17 +15,21 @@ def run_game():
     ai_settings = Settings()
     ai_settings.set_icon("alien_icon_32x32.bmp")
 
-    # cria o grupo de projéteis, todos disparados ficaram aqui
-    bullets_group = Group()
-
     # cria uma tela com as dimensões de ai_settings
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height)
     )
     pygame.display.set_caption("Alien Invasion")
 
+    # cria o grupo de projéteis, todos disparados ficaram aqui
+    bullets_group = Group()
+    aliens_group = Group()
+
     # cria a espaçonave
     ship = Ship(ai_settings, screen)
+
+    # cria a frota de aliens
+    gf.create_fleet(ai_settings, screen, aliens_group)
 
     # Inicia o laço principal do jogo
     # neste onde ocorre todos os eventos
@@ -41,7 +45,7 @@ def run_game():
         gf.update_bullets(bullets_group)
 
         # atualiza as informações da tela
-        gf.update_screen(ai_settings, screen, ship, bullets_group)
+        gf.update_screen(ai_settings, screen, ship, aliens_group, bullets_group)
 
 
 run_game()
--- game_funcions.py
+++ game_funcions.py
@@ -4,17 +4,18 @@ import sys
 import pygame
 
 from bullet import Bullet
+from alien import Alien
 
 
-def fire_bullet(settings, screen, ship, bullets_group):
+def fire_bullet(ai_settings, screen, ship, bullets_group):
     """Dispara um projétil se o limiete não for alcançado"""
     # cria um novo projétil e coloca-o no grupo
-    if len(bullets_group) < settings.bullets_allowed:
-        new_bullet = Bullet(settings, screen, ship)
+    if len(bullets_group) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
         bullets_group.add(new_bullet)
 
 
-def check_keydown_events(event, settings, screen, ship, bullets_group):
+def check_keydown_events(event, ai_settings, screen, ship, bullets_group):
     """ Responde somente a eventos quando aperta alguma tecla """
     if event == pygame.K_RIGHT:
         ship.moving_right = True
@@ -25,22 +26,24 @@ def check_keydown_events(event, settings, screen, ship, bullets_group):
     if event == pygame.K_DOWN:
         ship.moving_bottom = True
     if event == pygame.K_SPACE:
-        fire_bullet(settings, screen, ship, bullets_group)
+        fire_bullet(ai_settings, screen, ship, bullets_group)
 
 
-def check_keyup_events(event, ship):
+def check_keyup_events(event_key, ship):
     """ Responde somente a eventos quando solta alguma tecla """
-    if event == pygame.K_RIGHT:
+    if event_key == pygame.K_q or event_key == pygame.K_ESCAPE:
+        sys.exit()
+    if event_key == pygame.K_RIGHT:
         ship.moving_right = False
-    if event == pygame.K_LEFT:
+    if event_key == pygame.K_LEFT:
         ship.moving_left = False
-    if event == pygame.K_UP:
+    if event_key == pygame.K_UP:
         ship.moving_top = False
-    if event == pygame.K_DOWN:
+    if event_key == pygame.K_DOWN:
         ship.moving_bottom = False
 
 
-def check_events(settings, screen, ship, bullets_group):
+def check_events(ai_settings, screen, ship, bullets_group):
     """
     Realiza a escuta de eventos, evento de mouse e/ou teclado \n\r
     Todo evento de teclado é do tipo(type) KEYDOWN \n\r
@@ -50,21 +53,24 @@ def check_events(settings, screen, ship, bullets_group):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event.key, settings, screen, ship, bullets_group)
+            check_keydown_events(event.key, ai_settings, screen, ship, bullets_group)
 
         elif event.type == pygame.KEYUP:
             check_keyup_events(event.key, ship)
 
 
-def update_screen(settings, screen, ship, bullets_group):
+def update_screen(ai_settings, screen, ship, aliens_group, bullets_group):
     """Atualiza as imagens na tela, e gera/seta a mais recente"""
     # redesenha a tela
-    screen.fill(settings.bg_color)
-    ship.blitme()
+    screen.fill(ai_settings.bg_color)
 
     # redesenha todos os projéteis antes de atualizar a tela = flip()
     for bullet in bullets_group.sprites():
         bullet.draw_bullet()
+
+    # desenha a nave e o alien
+    ship.blitme()
+    aliens_group.draw(screen)
 
     # a presenta a tela mais recente
     pygame.display.flip()
@@ -81,3 +87,27 @@ def update_bullets(bullets_group):
     for bullet in bullets_group.copy():
         if bullet.rect.bottom <= 0:
             bullets_group.remove(bullet)
+
+
+def create_fleet(ai_settings, screen, aliens_group):
+    """
+    Cria uma frota de alienígena \n\r
+    Utiliza o primeiro criado para calcular a quantidade por linha e o esoaçamento \n\r
+    :param ai_settings: Objeto da classe Settings
+    :param screen: Objeto de retorne de pygame.display.set_mode()
+    :param aliens_group: objeto da classe Group() em pygame.sprite
+    """
+    # este é apenas para ter as medidas
+    alien = Alien(ai_settings, screen)
+    largura_alien = alien.rect.width
+    espaco_disponivel_x = ai_settings.screen_width - (2 * largura_alien)
+    numero_aliens_x = int(espaco_disponivel_x / (2 * largura_alien))
+
+    # cria a primeira linha de alienígenas
+    for numero_alien in range(numero_aliens_x):
+        # cria um alienígena e coloca n alinha
+        alien = Alien(ai_settings, screen)
+        alien.x = largura_alien + 2 * largura_alien * numero_alien
+        alien.rect.x = alien.x
+        # adiciona ao grupo de alienígenas
+        aliens_group.add(alien)
